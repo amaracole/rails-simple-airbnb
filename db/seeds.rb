fce9204c6e5d0f825521adf1256d83b43816da89
Amara Cole <amara.cole@yahoo.com>
2021-02-17 18:23:01 +0100

    done with the backend, now need to improve the frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  Flat.create(
+    name: Faker::FunnyName.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(70..99),
+    number_of_guests: rand(1..6)
+  )
+end
